d94b9f5ba57377488b3f9df8e6176315147f5912
Jinzhou Wu <jinzhouwu.public@gmail.com>
2023-02-11 19:31:00 -0500

    New Compose, Learn, and Article Pages

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const https = require("https");
-const getDate = require(__dirname + "/getDate.js");
+// const multer = require('multer');
+// const upload = multer();
 
 const admin = require("firebase-admin");
 const credentials = require(__dirname + "/key.json");
@@ -13,9 +14,16 @@ admin.initializeApp({
 const db = admin.firestore();
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.set('view engine', 'ejs');
+
+app.post('/api/compose', (req, res) => {
+
+    const response = db.collection("articles").add(req.body);
+
+    res.redirect("/learn");
+});
 
 app.get('/api/devData', async (req, res) => {
 
@@ -56,83 +64,43 @@ app.post('/api/devData/log', async (req, res) => {
     return await db.collection("devLog").add(req.body);
 });
 
-app.get("/test", (req, res) => {
-    res.send(__dirname);
-});
+app.get('/learn/:id', async (req, res) => {
 
-app.get("/learn", async (req, res) => {
-    const usersRef = db.collection("articles");
-    const response = await usersRef.get();
+    db.collection("articles").doc(req.params.id).get()
+        .then((doc) => {
+
+            if (doc.exists) {
+                const data = doc.data();
+                console.log(data);
+                //  res.send(data);
+                res.render('article', {article: data});
+            }
+            else  {
+                res.redirect("/404");
+            }
+        })
+        .catch((error) => {
+            res.redirect("/404");
+        });
+})
+
+app.get("/api/getArticles", async (req, res) => {
 
     let articlesList = [];
-    response.forEach((doc) => {
-        let article = doc.data();
-        article.id = doc.id;
-        articlesList.push(article);
-    })
 
-    res.render('learn', { articlesList: articlesList });
-});
-
-app.get("/compose", (req, res) => {
-    res.render('compose', { today: getDate.getDate() });
-});
-
-app.post("/compose", async (req, res) => {
-
-    const newArticle = {
-        title: req.body.title,
-        content: req.body.content,
-        publishDate: getDate.getDate(),
-        img_src: "https://schoolofathens.world/images/" + req.body.image
-    };
-
-    const response = await db.collection("articles").add(newArticle);
-
-    res.redirect("/learn");
-});
-
-app.get("/learn/:articleID", async (req, res) => {
-
-    var docRef = db.collection("articles").doc(req.params.articleID);
-
-    docRef.get().then((doc) => {
-        res.render('article', {article: doc.data()});
-    }).catch((error) => {
-        res.redirect("/404");
-    });
-});
-
-app.post("/api/test", (req, res) => {
-    res.send("The answer is " + (Number(req.body.num1) + Number(req.body.num2)));
-});
-
-app.post('/api/weather', (req, res) => {
-    const lat = req.body.latitude;
-    const lon = req.body.longitude;
-    const apiKey = "71c1776549fd5348791142a292b1db5d";
-    const unit = "metric";
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
-
-    console.log(url);
-
-    https.get(url, (response) => {
-        response.on("data", (data) => {
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const weatherDescription = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            res.write("<h1 style=\"text-align: center;\">The temperature is " + temp + " degree</h1>");
-            res.write("<p style=\"text-align: center;\">The weather is currently " + weatherDescription + "</p>");
-            res.write("<img src=\"" + imageURL + "\" style=\"display: block; margin-left: auto; margin-right: auto;\">");
-            res.send();
+    await db.collection("articles").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            const data = doc.data();
+            data.id = doc.id;
+            articlesList.push(data);
         });
     });
+
+    res.json({ articlesList: articlesList });
 });
 
 app.use((req, res, next) => {
     res.status(404).redirect("/404");
-  })
+});
 
 exports.app = functions.https.onRequest(app);
