c435ccf5c8e0d46e63c2ffe264b6c93e51d276ea
Jinzhou Wu <jinzhouwu.public@gmail.com>
2023-04-22 21:28:03 -0400

    add firebase config

---
+++ src/data/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIza" + "SyBjs" + "g82du" + "Vqy8N" + "wexQS" + "dQskI" + "hzv9N" + "9xx2A",
+  authDomain: "school-of-athens-122422" + ".firebaseapp.com",
+  projectId: "school-" + "of-athens-" + "122422",
+  storageBucket: "school-" + "of-athens-" + "122422" + ".appspot.com",
+  messagingSenderId: "767259" + "328870",
+  appId: "1:767259328870" + ":web:" + "cbce73385e22ed49e3c8a7",
+  measurementId: "G-THP2Y" + "BH90N",
+};
+
+export default firebaseConfig;
