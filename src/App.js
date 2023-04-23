27154a943e3f01f7e1ae7dcdf56691bb68b1560b
Jinzhou Wu <jinzhouwu.public@gmail.com>
2023-04-22 21:07:19 -0400

    add console log message

--- src/App.js
+++ src/App.js
@@ -35,6 +35,8 @@ export default function App() {
     document.title = "The School of Athens | Home to Modern Democracy";
   }, []);
 
+  console.log("Welcome to the School of Athens!");
+
   return (
     <BrowserRouter>
       <Routes>
